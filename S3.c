50501115cff682020486f7e167d7b3a9bbc4eb59
davidsubasu <david.subasu@gmail.com>
2026-03-04 19:15:21 +0200

    Create S3.c

---
+++ S3.c
@@ -0,0 +1,99 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+//trebuie sa folositi fisierul masini.txt
+//sau va creati un alt fisier cu alte date
+
+struct StructuraMasina {
+	int id;
+	int nrUsi;
+	float pret;
+	char* model;
+	char* numeSofer;
+	unsigned char serie;
+};
+typedef struct StructuraMasina Masina;
+
+void afisareMasina(Masina masina) {
+	printf("ID: %d\nNumar usi: %d\nPret: %f\nModel: %-10s\nNume sofer: %-10s\nSerie: %c\n\n", masina.id,masina.nrUsi, masina.pret, masina.model, masina.numeSofer,masina.serie);
+
+}
+
+void afisareVectorMasini(Masina* masini, int nrMasini) {
+	if (masini == NULL || nrMasini == 0) {
+		printf("Vectorul de masini este gol");
+		return;
+	}
+	for (int i = 0; i < nrMasini; i++) {
+		afisareMasina(masini[i]);
+	}
+}
+
+void adaugaMasinaInVector(Masina** masini, int * nrMasini, Masina masinaNoua) {
+	//if (nrMasini == NULL) return;
+	Masina* temp=(Masina*)malloc(sizeof(Masina)*((*nrMasini) + 1));
+	for (int i = 0; i < *nrMasini; i++) {
+		temp[i] = (*masini)[i];
+	}
+	temp[*nrMasini]=masinaNoua;
+	free(*masini);
+	*masini = temp;
+	(*nrMasini)++;
+}
+
+Masina citireMasinaFisier(FILE* file) {
+	char linie[256];
+	fgets(linie,255,file);
+	char delimitator[3] = ",\n";
+	Masina masina;
+	masina.id = atoi(strtok(linie,delimitator));
+	masina.nrUsi = atoi(strtok(NULL, delimitator));
+	masina.pret = atof(strtok(NULL, delimitator));
+	char* aux = (strtok(NULL, delimitator));
+	masina.model = (char*)malloc((strlen(aux) +1)* sizeof(char));
+	strcpy(masina.model,aux);
+	aux = (strtok(NULL, delimitator));
+	masina.numeSofer = (char*)malloc((strlen(aux) + 1) * sizeof(char));
+	strcpy(masina.numeSofer, aux);
+	masina.serie =  strtok(NULL, delimitator)[0];
+	return masina;
+}
+
+Masina* citireVectorMasiniFisier(const char* numeFisier, int* nrMasiniCitite) {
+	//functia primeste numele fisierului, il deschide si citeste toate masinile din fisier
+	//prin apelul repetat al functiei citireMasinaFisier()
+	//numarul de masini este determinat prin numarul de citiri din fisier
+	//ATENTIE - la final inchidem fisierul/stream-ul
+	FILE* file = fopen(numeFisier, "r");
+	if (!file) {
+		printf("Eroare la deschidere\n");
+		return;
+	}
+	else {
+		Masina* vectorMasini = NULL;
+		nrMasiniCitite = 0;
+		while (!feof(file)) {
+			
+			Masina masina = citireMasinaFisier(file);
+			adaugaMasinaInVector(&vectorMasini,nrMasiniCitite,masina);
+			return vectorMasini;
+		}
+	}
+}
+
+void dezalocareVectorMasini(Masina** vector, int* nrMasini) {
+	//este dezalocat intreg vectorul de masini
+
+}
+
+int main() {
+	int nrMasini = 0;
+
+	Masina* masini=citireVectorMasiniFisier("masini.txt",&nrMasini);
+	afisareVectorMasini(masini,nrMasini);
+
+	
+	return 0;
+}
